9d8c0a1719f284462152e5772476e6262cf89475
Lars Lorenz <lls-bin@web.de>
2016-06-03 01:00:21 +0200

    Add Frame recycler

--- Core.lua
+++ Core.lua
@@ -10,6 +10,31 @@ local Addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
 ---------------
 -- Functions --
 ---------------
-function Addon:Build()
+local function createAuraFrame()
+  local frame = CreateFrame("Frame")
+  frame.texture = frame:CreateTexture(nil, "BACKGROUND")
+  frame.texture:SetAllPoints()
+  frame.cooldown = CreateFrame("Cooldown", nil, frame, "CooldownFrameTemplate")
+  frame.cooldown:SetAllPoints()
+end
 
+local auraFrameCache = {}
+local function getAuraFrame()
+  local frame
+  local auraFrameCacheLength = #auraFrameCache
+  if #auraFrameCacheLength == 0 then
+    frame = createAuraFrame()
+  else
+    frame = auraFrameCache[#auraFrameCacheLength]
+    auraFrameCache[#auraFrameCacheLength] = nil
+  end
+  return frame
+end
+
+local function SetFrame()
+  
+end
+
+function Addon:Build()
+  
 end
